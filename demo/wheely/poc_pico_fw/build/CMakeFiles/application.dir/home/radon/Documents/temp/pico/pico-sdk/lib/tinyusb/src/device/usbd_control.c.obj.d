aaa524a56ac58b66cc17e927c9fbe72d2541da86
Marian Stoica <marianstoica128@gmail.com>
2023-03-11 18:24:40 +0200

    pico demo

---
+++ demo/wheely/poc_pico_fw/build/CMakeFiles/application.dir/home/radon/Documents/temp/pico/pico-sdk/lib/tinyusb/src/device/usbd_control.c.obj.d
@@ -0,0 +1,71 @@
+CMakeFiles/application.dir/home/radon/Documents/temp/pico/pico-sdk/lib/tinyusb/src/device/usbd_control.c.obj: \
+ /home/radon/Documents/temp/pico/pico-sdk/lib/tinyusb/src/device/usbd_control.c \
+ /home/radon/Documents/temp/pico/pico-sdk/lib/tinyusb/src/tusb_option.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/rp2_common/pico_stdio_usb/include/tusb_config.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/rp2_common/pico_stdio_usb/include/pico/stdio_usb.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/rp2_common/pico_stdio/include/pico/stdio.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/common/pico_base/include/pico.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/common/pico_base/include/pico/assert.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h \
+ /usr/include/newlib/assert.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ /home/radon/Documents/work/Projects/TBH-2023/metal/demo/wheely/poc_pico_fw/build/generated/pico_base/pico/version.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/common/pico_base/include/pico/config.h \
+ /home/radon/Documents/work/Projects/TBH-2023/metal/demo/wheely/poc_pico_fw/build/generated/pico_base/pico/config_autogen.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/boards/include/boards/pico.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/rp2_common/pico_platform/include/pico/platform.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/platform_defs.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/addressmap.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/sio.h \
+ /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/common/pico_base/include/pico/error.h \
+ /home/radon/Documents/temp/pico/pico-sdk/lib/tinyusb/src/tusb.h \
+ /home/radon/Documents/temp/pico/pico-sdk/lib/tinyusb/src/common/tusb_common.h \
+ /usr/include/newlib/string.h /usr/include/newlib/sys/reent.h \
+ /usr/include/newlib/_ansi.h /usr/include/newlib/sys/_types.h \
+ /usr/include/newlib/machine/_types.h /usr/include/newlib/sys/lock.h \
+ /usr/include/newlib/sys/_locale.h /usr/include/newlib/strings.h \
+ /usr/include/newlib/sys/string.h /usr/include/newlib/stdio.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdarg.h \
+ /usr/include/newlib/sys/types.h /usr/include/newlib/sys/_stdint.h \
+ /usr/include/newlib/machine/endian.h \
+ /usr/include/newlib/machine/_endian.h /usr/include/newlib/sys/select.h \
+ /usr/include/newlib/sys/_sigset.h /usr/include/newlib/sys/_timeval.h \
+ /usr/include/newlib/sys/timespec.h /usr/include/newlib/sys/_timespec.h \
+ /usr/include/newlib/sys/_pthreadtypes.h /usr/include/newlib/sys/sched.h \
+ /usr/include/newlib/machine/types.h /usr/include/newlib/sys/stdio.h \
+ /home/radon/Documents/temp/pico/pico-sdk/lib/tinyusb/src/common/tusb_compiler.h \
+ /home/radon/Documents/temp/pico/pico-sdk/lib/tinyusb/src/common/tusb_verify.h \
+ /home/radon/Documents/temp/pico/pico-sdk/lib/tinyusb/src/common/tusb_types.h \
+ /home/radon/Documents/temp/pico/pico-sdk/lib/tinyusb/src/common/tusb_error.h \
+ /home/radon/Documents/temp/pico/pico-sdk/lib/tinyusb/src/common/tusb_timeout.h \
+ /home/radon/Documents/temp/pico/pico-sdk/lib/tinyusb/src/osal/osal.h \
+ /home/radon/Documents/temp/pico/pico-sdk/lib/tinyusb/src/common/tusb_common.h \
+ /home/radon/Documents/temp/pico/pico-sdk/lib/tinyusb/src/osal/osal_pico.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/common/pico_time/include/pico/time.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/rp2_common/hardware_timer/include/hardware/timer.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/timer.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/rp2_common/hardware_base/include/hardware/address_mapped.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/timer.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/common/pico_sync/include/pico/sem.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/common/pico_sync/include/pico/lock_core.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/rp2_common/hardware_sync/include/hardware/sync.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/common/pico_sync/include/pico/mutex.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/common/pico_sync/include/pico/critical_section.h \
+ /home/radon/Documents/temp/pico/pico-sdk/lib/tinyusb/src/common/tusb_fifo.h \
+ /home/radon/Documents/temp/pico/pico-sdk/lib/tinyusb/src/osal/osal.h \
+ /home/radon/Documents/temp/pico/pico-sdk/lib/tinyusb/src/common/tusb_fifo.h \
+ /home/radon/Documents/temp/pico/pico-sdk/lib/tinyusb/src/device/usbd.h \
+ /home/radon/Documents/temp/pico/pico-sdk/lib/tinyusb/src/class/cdc/cdc_device.h \
+ /home/radon/Documents/temp/pico/pico-sdk/lib/tinyusb/src/class/cdc/cdc.h \
+ /home/radon/Documents/temp/pico/pico-sdk/lib/tinyusb/src/device/usbd_pvt.h \
+ /home/radon/Documents/temp/pico/pico-sdk/lib/tinyusb/src/device/dcd.h \
+ /home/radon/Documents/temp/pico/pico-sdk/lib/tinyusb/src/device/dcd_attr.h
---
+++ demo/wheely/poc_pico_fw/build/CMakeFiles/application.dir/home/radon/Documents/temp/pico/pico-sdk/src/rp2_common/pico_float/float_init_rom.c.obj.d
@@ -0,0 +1,32 @@
+CMakeFiles/application.dir/home/radon/Documents/temp/pico/pico-sdk/src/rp2_common/pico_float/float_init_rom.c.obj: \
+ /home/radon/Documents/temp/pico/pico-sdk/src/rp2_common/pico_float/float_init_rom.c \
+ /usr/include/newlib/string.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h /usr/include/newlib/sys/reent.h \
+ /usr/include/newlib/_ansi.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/sys/_locale.h /usr/include/newlib/strings.h \
+ /usr/include/newlib/sys/string.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/rp2_common/pico_bootrom/include/pico/bootrom.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/common/pico_base/include/pico.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/common/pico_base/include/pico/assert.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h \
+ /usr/include/newlib/assert.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /home/radon/Documents/work/Projects/TBH-2023/metal/demo/wheely/poc_pico_fw/build/generated/pico_base/pico/version.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/common/pico_base/include/pico/config.h \
+ /home/radon/Documents/work/Projects/TBH-2023/metal/demo/wheely/poc_pico_fw/build/generated/pico_base/pico/config_autogen.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/boards/include/boards/pico.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/rp2_common/pico_platform/include/pico/platform.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/platform_defs.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/addressmap.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/sio.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/common/pico_base/include/pico/error.h \
+ /home/radon/Documents/temp/pico/pico-sdk/src/rp2_common/pico_bootrom/include/pico/bootrom/sf_table.h
